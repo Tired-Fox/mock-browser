c99e2b6f071ef5e0371bec62a5efdf918d3ab01b
Zachary Boehm <zboehm@menard-inc.com>
2022-09-12 16:02:00 -0500

    Made left menu collapsable

--- src/components/browser/Browser.jsx
+++ src/components/browser/Browser.jsx
@@ -2,11 +2,16 @@ import { h, Fragment } from 'preact';
 import { useState } from 'preact/hooks';
 import './browser.scss';
 
+const HOME = 'https://www.google.com/webhp?igu=1&safe=active&ssui=on';
+const SEARCH = 'https://www.google.com/search?igu=1&ei=&q=';
+
+let leftMenu = true;
+
 function formatURI(uri) {
     if (['.', '/'].some(value => uri.includes(value))) {
         return `http://${uri}`;
     } else {
-        return `https://www.google.com/search?igu=1&ei=&q=${encodeURIComponent(uri)}`;
+        return `${SEARCH}${encodeURIComponent(uri)}`;
     }
 }
 
@@ -14,17 +19,54 @@ export function Content({ ...props }) {
     console.log(props.url);
     return (
         <>
-            <iframe
-                id="content-iframe"
-                src={formatURI(props.url)}></iframe>
+            <div id="content">
+                <a href="#" id="content-open-leftMenu"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        document.getElementById('left-ui').classList.remove('d-none');
+                        document.getElementById('content-open-leftMenu').classList.add('d-none');
+                        leftMenu = true;
+                    }}>
+                    <i class="fa-regular fa-square-caret-right normal"></i>
+                </a>
+                <iframe
+                    id="content-iframe"
+                    src={props.url}>
+                </iframe>
+
+            </div>
+
         </>
     );
 }
 
 export function LeftBar({ ...props }) {
     let url = '';
+
     return (
-        <div class="left-ui">
+        <div id="left-ui">
+            <div class="left-controls">
+                <div>
+                    <a href="#" onClick={(e) => {
+                        e.preventDefault();
+                        history.back();
+                    }}>
+                        <p>&lt;</p>
+                    </a>
+                    <a href="#" onClick={(e) => {
+                        e.preventDefault();
+                        history.forward();
+                    }}>
+                        <p>&gt;</p>
+                    </a>
+                </div>
+                <button onClick={(e) => {
+                    document.getElementById('left-ui').classList.add('d-none');
+                    document.getElementById('content-open-leftMenu').classList.remove('d-none');
+                }}>
+                    <i class="fa-regular fa-square-caret-left normal"></i>
+                </button>
+            </div>
             <input
                 id="search-bar"
                 class="small"
@@ -35,7 +77,7 @@ export function LeftBar({ ...props }) {
                 onChange={(e) => {
                     url = e.target.value;
                     if (url) {
-                        props.searchHook(url);
+                        props.searchHook(formatURI(url));
                         url = ''
                     }
                 }}
@@ -45,7 +87,8 @@ export function LeftBar({ ...props }) {
 }
 
 export function Browser({ ...props }) {
-    const [url, setUrl] = useState('www.google.com/webhp?igu=1&safe=active&ssui=on');
+    const [url, setUrl] = useState(HOME);
+
     const newUrl = (url) => {
         console.log(url);
         setUrl(url);
